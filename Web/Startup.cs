75208829d831c519348a72fe8736ac8703d49aa7
nsatishnaidu <satish.naidu@systemc.com>
2021-09-15 17:28:21 +0000

    Added the web project

---
+++ Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Linker.Web.Configuration;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Web.Startup))]
+
+namespace Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.ConfigureWebApi();
+        }
+    }
+}
